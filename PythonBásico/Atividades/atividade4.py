468fdd2f07527e7cf556951fe57bd9a79f65b4ad
maxwelrobson <maxps2@live.com>
2026-07-02 15:59:59 -0300

    primeiro commit

---
+++ PythonBásico/Atividades/atividade4.py
@@ -0,0 +1,5 @@
+# Elabore um programa em Python para calcular média de quatro números e exiba o resultado para o usuário.
+# Use as seguintes notas como entrada de dados do seu programa: 8, 9, 10 e 5.
+
+media = (8 + 9 + 10 + 5) / 4
+print(media)
---
+++ PythonBásico/exemplo1.py
@@ -0,0 +1,18 @@
+# Variáveis
+idade = 25
+nome = "João"
+altura = 1.75
+
+# Exibir Dados
+print("Nome:", nome)
+print("Idade:", idade)
+print("Altura:", altura)
+
+# Manipular de Dados
+idade = 26
+
+# Controle de Fluxo
+if idade >= 18:
+    print (nome, "é maior de idade.")
+else:
+    print (nome, "é menor de idade.")
