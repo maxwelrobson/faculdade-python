91e50fdf962a7f05a4ab50221f415e456b15010d
maxwelrobson <maxps2@live.com>
2026-07-08 15:18:09 -0300

    Tema 2 - Python Básico

---
+++ PythonBásico/exemplo3.py
@@ -0,0 +1,11 @@
+#Defina uma função chamada imprimir_dados que aceita dois parâmetros: um número e uma string.
+#Dentro da função, imprima o número e a string usando a função print().
+#Forneça dois valores para a função imprimir_dados: um número e uma string.
+#Chame a função imprimir_dados com os valores fornecidos.
+
+def imprimir_dados(numero, texto):
+    print("Número:", numero)
+    print("Texto:", texto)
+
+
+imprimir_dados(10, "Max")
